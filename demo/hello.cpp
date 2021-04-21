9b3a40f59dae66a8fdccb03814a1541883aa4a19
YuanL12 <yuanluo@uchicago.edu>
2021-04-21 16:13:06 +0800

    include updating persistence function in include/plu.hpp

--- demo/hello.cpp
+++ demo/hello.cpp
@@ -1,16 +1,58 @@
 #include <bats.hpp>
 #include <plu.hpp>
+#include <vector>
+
+#define F2 ModP<int, 2>
+#define F3 ModP<int, 3>
 
 int main() {
 
     // you can use BATS
-    bats::SimplicialComplex X;
-    X.add_recursive({0,1});
-    X.print_summary();
+    //bats::SimplicialComplex X;
+    //X.add_recursive({0,1});
+    //X.print_summary();
+    
+	// How to initialzie a sparse matrix
+    //std::vector<size_t> colptr{0, 2, 3, 6};
+    //std::vector<size_t> rowind{0, 2, 2, 0, 1, 2};
+    //std::vector<int> val{1, 2, 3, 4, 5, 6};
+    //CSCMatrix<int> M(3,3,colptr, rowind, val);
+    //M.print(0,3,0,3);
+    
+	
+	using VT = SparseVector<F2, size_t>;
+	using MatT = ColumnMatrix<VT>;
+	//sparse vectos are specified by index and its value(nonzero) 
+	std::vector<VT> cols{VT({0},{1}), VT({1,2},{1,1}), VT({0,2},{1,1})};
+	
+	//intialize a matrix
+	MatT B(4, 3, cols);
+	//B.swap_cols(0,1);//swap columns
+	//B.print();//print columns
+	MatT R(B);
+	MatT U = MatT::identity(B.ncol());
+	bats::reduce_matrix(R, U);
 
-    // you can use functions from your
-    // repository include headers
-    hello_PLU();
+	//check all results
+	if (U.is_upper())
+		if (R.is_reduced())
+			if (R * u_inv(U) == B)
+				std::cout << "before permuation, success"<< '\n';
 
-    return 0;
+	//swap columns of row permutation matrix will result row change of 
+	//the original one
+	MatT row_perm(MatT::identity(R.nrow()));
+	row_perm.swap_cols(1,2);
+	
+	//swap columns of column permutation matrix will result same 
+	//effect on the column of original matrix
+	MatT col_perm(MatT::identity(R.ncol()));
+    col_perm.swap_cols(0,1);
+	
+	update_reduction<VT>(R, U, row_perm, col_perm);
+
+	if (R * u_inv(U) == row_perm * B * col_perm){
+		std::cout << "after permuation, success"<< '\n';
+	}
+	return 0;
 }
--- include/plu.hpp
+++ include/plu.hpp
@@ -5,6 +5,31 @@ header file for PLU factorization
 #include <bats.hpp>
 #include <iostream>
 
-void hello_PLU() {
-        std::cout << "hello from PLU.hpp" <<std::endl;
+
+void hello_PLU(){
+	std::cout << "hello from PLU.hpp" <<std::endl;
+}
+
+template <class TC>
+void update_reduction(ColumnMatrix<TC> &R_k, ColumnMatrix<TC> &U_k, 
+const ColumnMatrix<TC> & row_perm, 
+const ColumnMatrix<TC> &column_perm){
+	
+	using MatT = ColumnMatrix<TC>;
+	MatT P_k_minus_1 = row_perm;
+	MatT P_k = column_perm;
+	    
+	auto F = UQL(P_k.T() * U_k);                                                
+    MatT U = F.U;        
+    MatT L = F.L;
+    MatT Q = F.E;
+
+    MatT R_k_prime = P_k_minus_1 * R_k * l_inv(L) * Q.T();
+    MatT U_double_prime = MatT::identity(R_k_prime.ncol());
+    bats::reduce_matrix(R_k_prime, U_double_prime);
+
+    MatT U_k_prime = U * U_double_prime;
+
+    R_k = R_k_prime;
+    U_k = U_k_prime;    
 }
