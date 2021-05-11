c9b43f262e79cbc52fd903b4f48a474f20151a08
YuanL12 <yuanluo@uchicago.edu>
2021-05-11 15:02:22 +0800

    add the list permutation version of updating persistence function

--- demo/updating_persistence_test.cpp
+++ demo/updating_persistence_test.cpp
@@ -12,46 +12,76 @@ int main() {
     //X.add_recursive({0,1});
     //X.print_summary();
     
-	// How to initialzie a sparse matrix
-    //std::vector<size_t> colptr{0, 2, 3, 6};
-    //std::vector<size_t> rowind{0, 2, 2, 0, 1, 2};
-    //std::vector<int> val{1, 2, 3, 4, 5, 6};
-    //CSCMatrix<int> M(3,3,colptr, rowind, val);
-    //M.print(0,3,0,3);
-    
-	
 	using VT = SparseVector<F2, size_t>;
 	using MatT = ColumnMatrix<VT>;
 	//sparse vectos are specified by index and its value(nonzero) 
 	std::vector<VT> cols{VT({0},{1}), VT({1,2},{1,1}), VT({0,2},{1,1})};
-	
-	//initialize a matrix
-	MatT B(4, 3, cols);
-	//B.swap_cols(0,1);//swap columns
-	//B.print();//print columns
-	MatT R(B);
-	MatT U = MatT::identity(B.ncol());
-	bats::reduce_matrix(R, U);
 
-	//check all results
-	if (U.is_upper())
-		if (R.is_reduced())
-			if (R * u_inv(U) == B)
-				std::cout << "before permutation, success"<< '\n';
+	{
+		std::cout << "\nstart matrix permuation" << std::endl;
+		MatT B(4, 3, cols); //initialize a boundary matrix
+		MatT R(B);
+		MatT U = MatT::identity(B.ncol());
+		bats::reduce_matrix(R, U);
 
-	//swap columns of row permutation matrix will result row change of 
-	//the original one
-	MatT row_perm(MatT::identity(R.nrow()));
-	row_perm.swap_cols(1,2);
+		//check all results
+		if (U.is_upper())
+			if (R.is_reduced())
+				if (R * u_inv(U) == B)
+					std::cout << "before permutation, success"<< '\n';
+
+		//swap columns of row permutation matrix will result row change of 
+		//the original one
+		MatT row_perm_mat(MatT::identity(R.nrow()));
+		row_perm_mat.swap_cols(1,2);
+		std::vector<size_t> row_perm{0,2,1};
+		//swap columns of column permutation matrix will result same 
+		//effect on the column of original matrix
+		MatT col_perm_mat(MatT::identity(R.ncol()));
+		col_perm_mat.swap_cols(0,1);
+		std::vector<size_t> col_perm{1,0,2};
+		std::cout << "before matrix permutation R is" << std::endl;
+		R.print();
 	
-	//swap columns of column permutation matrix will result same 
-	//effect on the column of original matrix
-	MatT col_perm(MatT::identity(R.ncol()));
-    col_perm.swap_cols(0,1);
-	
-	update_reduction<VT>(R, U, row_perm, col_perm);
-	if (R * u_inv(U) == row_perm * B * col_perm){
-		std::cout << "after permutation, success"<< '\n';
+		update_reduction<VT>(R, U, row_perm_mat, col_perm_mat);
+		if (R * u_inv(U) == row_perm_mat * B * col_perm_mat){
+			std::cout << "using matrix permutation, success"<< '\n';
+		}
+		std::cout << "after matrix permutation R is" << std::endl;
+		R.print();
 	}
+
+	{
+		std::cout << "\nstart list permuation" << std::endl;
+		MatT B(4, 3, cols); //initialize a boundary matrix
+		MatT R(B);
+		MatT U = MatT::identity(B.ncol());
+		bats::reduce_matrix(R, U);
+
+		//check all results
+		if (U.is_upper())
+			if (R.is_reduced())
+				if (R * u_inv(U) == B)
+					std::cout << "before permutation, success"<< '\n';
+
+		//swap columns of row permutation matrix will result row change of 
+		//the original one
+		std::vector<size_t> row_perm{0,2,1};
+		//swap columns of column permutation matrix will result same 
+		//effect on the column of original matrix
+		std::vector<size_t> col_perm{1,0,2};
+
+		std::cout << "before list permutation R is" << std::endl;
+		R.print();
+		update_reduction<VT>(R, U, row_perm, col_perm);
+		B.permute_cols(col_perm);
+		B.permute_rows(row_perm);
+		if (R * u_inv(U) == B ){
+			std::cout << "using list permutation, success"<< '\n';
+		}
+		std::cout << "after list permutation R is" << std::endl;
+		R.print();
+	}
+
 	return 0;
 }
--- include/plu.hpp
+++ include/plu.hpp
@@ -4,16 +4,44 @@ header file for PLU factorization
 #pragma once
 #include <bats.hpp>
 #include <iostream>
+#include <vector>
 
-
-void hello_PLU(){
-	std::cout << "hello from PLU.hpp" <<std::endl;
+template <typename CpxT, typename T>
+void print_summary_of_filtration(const CpxT& X, std::function<T(const std::vector<size_t>&)>& filtfn){
+    std::cout << "\nLet's see the filtration value on each dimension" << std::endl;
+    for (size_t k = 0; k < X.maxdim() + 1; k++) {
+        std::cout << "For dimension "<< k << std::endl;
+        for (auto& s : X.get_simplices(k)) {
+            std::cout << "simplex with index "<< X.find_idx(s) << ", filtration value f(s) is " << filtfn(s) << std::endl; 
+        }
+    }
 }
 
+
+//two ways to display 2D vectors
+template<typename T>
+void print_2D_vectors (const T& perms) {
+//perms represents permutations
+    for (auto& valsk: perms) {
+        for (auto& v: valsk) {
+            std::cout << v << " ";
+        }
+        std::cout << "\n";
+    }
+}
+template<typename T>
+void print_1D_vectors (const T& perm) {
+    for (auto& v: perm) {
+        std::cout << v << " ";
+    }
+    std::cout << "\n";
+}
+
+// Reduction algorithm of matrix permutation version 
+// of updating persistence by passing old RU factorzation 
+// and row and column permutations 
 template <class TC>
-void update_reduction(ColumnMatrix<TC> &R_k, ColumnMatrix<TC> &U_k, 
-const ColumnMatrix<TC> & row_perm, 
-const ColumnMatrix<TC> &column_perm){
+void update_reduction(ColumnMatrix<TC> &R_k, ColumnMatrix<TC> &U_k, const ColumnMatrix<TC> & row_perm, const ColumnMatrix<TC> &column_perm){
 	
 	using MatT = ColumnMatrix<TC>;
 	MatT P_k_minus_1 = row_perm;
@@ -25,6 +53,30 @@ const ColumnMatrix<TC> &column_perm){
     MatT Q = F.E;
 
     MatT R_k_prime = P_k_minus_1 * R_k * l_inv(L) * Q.T();
+    MatT U_double_prime = MatT::identity(R_k_prime.ncol());
+    bats::reduce_matrix(R_k_prime, U_double_prime);
+
+    MatT U_k_prime = U * U_double_prime;
+
+    R_k = R_k_prime;
+    U_k = U_k_prime;    
+}
+
+// Reduction algorithm of list permutation version(maybe faster)
+// for updating persistence by passing old RU factorzation 
+// and row and column permutations (in the form of list permutation)
+template <class TC>
+void update_reduction(ColumnMatrix<TC> &R_k, ColumnMatrix<TC> &U_k, const std::vector<size_t> &row_perm, const std::vector<size_t> &column_perm){
+	
+	using MatT = ColumnMatrix<TC>;
+    U_k.permute_rows(bats::util::inv_perm(column_perm));
+	auto F = UQL(U_k);                                                
+    MatT U = F.U;        
+    MatT L = F.L;
+    MatT Q = F.E;
+
+    MatT R_k_prime = R_k * l_inv(L) * Q.T();
+    R_k_prime.permute_rows(row_perm);
     MatT U_double_prime = MatT::identity(R_k_prime.ncol());
     bats::reduce_matrix(R_k_prime, U_double_prime);
 
